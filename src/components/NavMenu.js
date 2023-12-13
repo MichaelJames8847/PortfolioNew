fb4c37f24b0af6a1a35f508c0310914404e739ff
Michael James <jmj6ggggg@gmail.com>
2023-12-13 14:13:00 -0600

    Edit styling

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,15 +4,14 @@ import { Navbar, NavbarBrand, Nav, NavLink } from 'reactstrap';
 import './NavMenu.css';
 
 export const NavMenu = () => {
-
   return (
-      <Navbar className="navbar-main" dark expand="md">
-        <NavbarBrand>Michael James</NavbarBrand>
-          <Nav navbar className='ml-auto'>
-            <NavLink tag={RRNavLink} to="/" activeclassname="active">Home</NavLink>
-            <NavLink tag={RRNavLink} to="/blog" activeclassname="active">Blogs</NavLink>
-            <NavLink tag={RRNavLink} to="/about" activeclassname="active">About Me</NavLink>
-          </Nav>
-      </Navbar>
+    <Navbar className="navbar-main" dark expand="md"> {/* Enable responsiveness */}
+      <NavbarBrand>Michael James</NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavLink tag={RRNavLink} to="/" activeclassname="active">Home</NavLink>
+        <NavLink tag={RRNavLink} to="/blog" activeclassname="active">Blogs</NavLink>
+        <NavLink tag={RRNavLink} to="/about" activeclassname="active">About Me</NavLink>
+      </Nav>
+    </Navbar>
   );
 }
